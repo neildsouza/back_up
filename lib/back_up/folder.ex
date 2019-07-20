afa320c5d61a3e81e959d4a216304c30c2618dbf
Neil D'souza <neil@freesoftsolutions.co.in>
2019-07-20 21:56:54 +0530

    Add file_copy_proc.ex

--- lib/back_up/folder.ex
+++ lib/back_up/folder.ex
@@ -95,65 +95,26 @@ defmodule BackUp.Folder do
       IO.puts("Folder: #{state.current_folder}, Files: #{length(state.files)}")
       Enum.each(state.files, fn(file_path) ->
 	Enum.each(state.backup_dst_folders, fn(backup_dst_folder) ->
-	  Task.start(fn ->
-	    dst_path = String.replace(
-	      file_path,
-	      state.start_folder,
-	      backup_dst_folder.backup_folder
-            )
-
-	    if File.exists?(dst_path) do
-	      case File.cp(file_path, dst_path, &cp_file/2) do
-		:ok ->
-		  write_file_stats(file_path, dst_path)
-		{:error, reason} ->
-		  msg = """
-		     Msg: Error backing up file #{file_path} to #{dst_path}
-		  Reason: #{inspect reason}
-		  """	
-		  IO.puts(msg)
-	      end
-	    else
-	      case File.cp(file_path, dst_path) do
-		:ok ->
-		  write_file_stats(file_path, dst_path)
-		  IO.puts("#{file_path} --> #{dst_path}")
-		{:error, reason} ->
-		  msg = """
-		     Msg: Error backing up file #{file_path} to #{dst_path}
-		  Reason: #{inspect reason}
-		  """
-		  IO.puts(msg)
-	      end
-	    end
-	  end)
+	  {:ok, pid} =
+	    DynamicSupervisor.start_child(
+	      BackUp.FilesystemSup,
+	      {
+		BackUp.FileCopyProc,
+		%{
+		  src_file: file_path,
+		  start_folder: state.start_folder,
+		  backup_folder: backup_dst_folder.backup_folder
+		}
+	      }
+	    )
+	  BackUp.Folder.crawl_folder(pid)
 	end)
       end)
     else
       IO.puts("Folder: #{state.current_folder}, Files: #{length(state.files)}")
     end
   end
-
-  defp cp_file(src_file, dst_file) do
-    src_hash_task = Task.async(fn ->
-      Filesystem.hash_content(src_file)
-    end)
-
-    dst_hash_task = Task.async(fn ->
-      Filesystem.hash_content(dst_file)
-    end)
-
-    src_hash = Task.await(src_hash_task, :infinity)
-    dst_hash = Task.await(dst_hash_task, :infinity)
-    
-    unless src_hash  == dst_hash do
-      IO.puts("#{src_file} --> #{dst_file}")
-      true
-    else
-      false
-    end
-  end
-
+  
   defp create_folder(dst_folder) do
     unless File.exists?(dst_folder) do
       case File.mkdir_p(dst_folder) do
@@ -167,10 +128,5 @@ defmodule BackUp.Folder do
 	  IO.puts(msg)
       end
     end
-  end
-
-  defp write_file_stats(file_path, dst_path) do
-    file_path_stat = File.stat!(file_path, time: :posix)
-    File.write_stat(dst_path, file_path_stat, time: :posix)
   end
 end
