03ccf167ac171b9dee7b414803a22598125f6e36
Neil D'souza <neil@freesoftsolutions.co.in>
2019-07-27 17:18:15 +0530

    Clean up timer msgs

--- lib/back_up/util.ex
+++ lib/back_up/util.ex
@@ -1,25 +1,25 @@
 defmodule BackUp.Util do
   def convert(secs) when 0 <= secs and secs < 60 do
-    "0 hrs 0 mins #{secs} secs"
+    "0 hr(s) 0 mins #{secs} secs"
   end
 
   def convert(secs) when secs == 60 do
-    "0 hrs 1 min 0 secs"
+    "0 hr(s) 1 min 0 secs"
   end
 
   def convert(secs) when 60 < secs and secs < 3600 do
     mins = div(secs, 60)
     s = rem(secs, 60)
-    "0 hrs #{mins} min(s) #{s} sec(s)"
+    "0 hr(s) #{mins} min(s) #{s} sec(s)"
   end
 
   def convert(secs) when secs == 3600 do
-    "1 hr 0 mins 0 secs"
+    "1 hr(s) 0 mins 0 secs"
   end
 
   def convert(secs) when secs > 3600 do
     h = div(secs, 3600)
     r = convert(rem(secs, 3600))
-    "#{h} hrs " <> String.replace(r, "0 hrs", "")
+    "#{h} hr(s) " <> String.replace(r, "0 hr(s)", "")
   end
 end
